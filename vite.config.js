8528203a19dd348dddda4953709f2a27ce0eec04
Artyom <awesomemdl95@gmail.com>
2023-01-29 16:46:36 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,10 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [vue()],
+
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 
   css: {
     modules: {
