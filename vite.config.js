71cc7f17efa51cdb7ca505fa0f469397e08b5bc5
Artyom <awesomemdl95@gmail.com>
2023-04-12 13:51:59 +0300

    pwa branch

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,47 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const vitePWA = VitePWA(
+	{
+		registerType: 'autoUpdate',
+		outDir: 'dist',
+		manifest: {
+			name: 'utms generator',
+			short_name: 'utms',
+			description: 'App for generate utm parameters. Usefull for marketers.',
+			theme_color: '#ffffff',
+			icons: [
+				{
+					src: 'images/favicon-32x32.png',
+					sizes: '32x32',
+					type: 'image/png'
+				},
+				{
+					src: 'images/android-chrome-192x192.png',
+					sizes: '192x192',
+					type: 'image/png'
+				},
+				{
+					src: 'images/android-chrome-512x512.png',
+					sizes: '512x512',
+					type: 'image/png'
+				}
+			]
+		}
+	}
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    open: true,
-    port: 3000,
-  },
-  plugins: [vue()],
+	server: {
+		open: true,
+		port: 3000,
+	},
+	plugins: [vue(), vitePWA],
 
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-
-  css: {
-    modules: {
-      generateScopedName: '[local]__[hash:base64:6]',
-      localsConvention: 'camelCase',
-    },
-  },
+	resolve: {
+		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+	},
 });
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+export default {
+	globDirectory: 'dist/',
+	globPatterns: [
+		'**/*/{css,woff2,png,svg,jpg,jpeg,js}'
+	],
+	swDest: 'build/sw.js'
+};
